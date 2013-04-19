666fb6e45fce711340836db5c492a724a1b42a26
Axel Lin <axel.lin@gmail.com>
2013-04-19 16:39:29 -0500

    misc: fsa9485: convert to use themodule_i2c_driver() macro
    * Bring in line with caf&korg 3.4.y fsa9480 driver
    
    MISC: convert drivers/misc/* to use module_i2c_driver()
    
    This patch converts the drivers in drivers/misc/* to use the
    module_i2c_driver() macro which makes the code smaller and a bit
    simpler.
    
    Signed-off-by: Axel Lin <axel.lin@gmail.com>
    Cc: Michael Hennerich <hennerich@blackfin.uclinux.org>
    Cc: Anantha Narayanan <Anantha.Narayanan@intel.com>
    Cc: Hemanth V <hemanthv@ti.com>
    Cc: Christoph Mair <christoph.mair@gmail.com>
    Cc: Grant Likely <grant.likely@secretlab.ca>
    Cc: Ben Gardner <bgardner@wabtec.com>
    Cc: Minkyu Kang <mk7.kang@samsung.com>
    Cc: Kalhan Trisal <kalhan.trisal@intel.com>
    Cc: Darrick J. Wong <djwong@us.ibm.com>
    Cc: Daniel Mack <zonque@gmail.com>
    Cc: Rodolfo Giometti <giometti@linux.it>
    Acked-by: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>
    
    misc: fsa9480: Remove obsolete cleanup for clientdata
    
    A few new i2c-drivers came into the kernel which clear the clientdata-pointer
    on exit or error. This is obsolete meanwhile, the core will do it.
    
    Change-Id: Ie172160e0b9241beca7bd22f38087070d15e6ec9
    Signed-off-by: Wolfram Sang <w.sang@pengutronix.de>
    Acked-by: Kyungmin Park <kyungmin.park@samsung.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/misc/fsa9485.c
+++ drivers/misc/fsa9485.c
@@ -1299,7 +1299,6 @@ fail2:
 fail1:
 	input_unregister_device(input);
 	mutex_destroy(&usbsw->mutex);
-	i2c_set_clientdata(client, NULL);
 	kfree(usbsw);
 	return ret;
 }
@@ -1314,7 +1313,6 @@ static int __devexit fsa9485_remove(struct i2c_client *client)
 		free_irq(client->irq, usbsw);
 	}
 	mutex_destroy(&usbsw->mutex);
-	i2c_set_clientdata(client, NULL);
 
 	sysfs_remove_group(&client->dev.kobj, &fsa9485_group);
 	kfree(usbsw);
@@ -1355,17 +1353,7 @@ static struct i2c_driver fsa9485_i2c_driver = {
 	.id_table = fsa9485_id,
 };
 
-static int __init fsa9485_init(void)
-{
-	return i2c_add_driver(&fsa9485_i2c_driver);
-}
-module_init(fsa9485_init);
-
-static void __exit fsa9485_exit(void)
-{
-	i2c_del_driver(&fsa9485_i2c_driver);
-}
-module_exit(fsa9485_exit);
+module_i2c_driver(fsa9485_i2c_driver);
 
 MODULE_AUTHOR("Minkyu Kang <mk7.kang@samsung.com>");
 MODULE_DESCRIPTION("FSA9485 USB Switch driver");
