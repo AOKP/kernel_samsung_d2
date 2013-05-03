34883d65d8406ec02936a42c4fe13f5a7d323850
bmc08gt <bmc08gt.teamb00sted@gmail.com>
2013-05-04 01:40:42 +0200

    d2:block: Tune ROW iosched params
    Change-Id: I429ed4052305c1fee75c9192e5387a4e9baa8a02
    Signed-off-by: bmc08gt <bmc08gt.teamb00sted@gmail.com>

--- block/row-iosched.c
+++ block/row-iosched.c
@@ -86,17 +86,17 @@ struct row_queue_params {
  */
 static const struct row_queue_params row_queues_def[] = {
 /* idling_enabled, quantum, is_urgent */
-	{true, 10, true},	/* ROWQ_PRIO_HIGH_READ */
-	{false, 1, false},	/* ROWQ_PRIO_HIGH_SWRITE */
-	{true, 100, true},	/* ROWQ_PRIO_REG_READ */
-	{false, 1, false},	/* ROWQ_PRIO_REG_SWRITE */
-	{false, 1, false},	/* ROWQ_PRIO_REG_WRITE */
-	{false, 1, false},	/* ROWQ_PRIO_LOW_READ */
-	{false, 1, false}	/* ROWQ_PRIO_LOW_SWRITE */
+	{true, 100, true},	/* ROWQ_PRIO_HIGH_READ */
+	{false, 6, false},	/* ROWQ_PRIO_HIGH_SWRITE */
+	{true, 70, true},	/* ROWQ_PRIO_REG_READ */
+	{false, 5, false},	/* ROWQ_PRIO_REG_SWRITE */
+	{false, 5, false},	/* ROWQ_PRIO_REG_WRITE */
+	{false, 3, false},	/* ROWQ_PRIO_LOW_READ */
+	{false, 3, false}	/* ROWQ_PRIO_LOW_SWRITE */
 };
 
 /* Default values for idling on read queues (in msec) */
-#define ROW_IDLE_TIME_MSEC 5
+#define ROW_IDLE_TIME_MSEC 12
 #define ROW_READ_FREQ_MSEC 20
 
 /**
